71e0838f6816de09bab27ea3d36dee6417ed404d
hcylia <36785991+hcylia@users.noreply.github.com>
2021-05-09 18:03:41 +0200

    first commit

---
+++ Assets/scripts/HighscoreText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class HighscoreText : MonoBehaviour
+{
+    Text highscore;
+
+    //void Start()
+    //{
+    //    highscore = GetComponent<Text>();
+    //    highscore.text = "High score: " + PlayerPrefs.GetInt("HighScore").ToString();
+    //}
+    private void Update()
+    {
+        highscore = GetComponent<Text>();
+        highscore.text = "High score: " + PlayerPrefs.GetInt("HighScore").ToString();
+    }
+}
---
+++ Assets/scripts/TapController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TapController : MonoBehaviour
+{
+    public delegate void PlayerDelegate();
+    public static event PlayerDelegate OnPlayerDied;
+    public static event PlayerDelegate OnPlayerScored;
+
+    public float tapForce = 10;
+    public float tiltSmooth = 5;
+    public Vector3 startPos;
+
+    Rigidbody2D rigidbody;
+    Quaternion downRotation;
+    Quaternion forwardRotation;
+    GameManager game;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        downRotation = Quaternion.Euler(0, 0, -90);
+        forwardRotation = Quaternion.Euler(0, 0, 35);
+        game = GameManager.Instance;
+        rigidbody.simulated = false;
+    }
+    private void OnEnable()
+    {
+        GameManager.OnGameStarted += OnGameStarted;
+        GameManager.OnGameOverConfirmed += OnGameOverConfirmed;
+    }
+    private void OnDisable()
+    {
+        GameManager.OnGameStarted -= OnGameStarted;
+        GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;
+    }
+    void OnGameStarted()
+    {
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.simulated = true;
+    }
+    void OnGameOverConfirmed()
+    {
+        transform.localPosition = startPos;
+        transform.rotation = Quaternion.identity;
+        rigidbody.simulated = false;
+    }
+    private void Update()
+    {
+        if (game.GameOver) return;
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Time.timeScale += 1;
+            transform.rotation = forwardRotation;
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+        }
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "scoreZone")
+        {
+            //Register a score event
+            OnPlayerScored(); //event sent to GameManager;
+            //play a sound
+        }
+        if (collision.gameObject.tag == "deadZone")
+        {
+            rigidbody.simulated = false;
+            //Register a dead event
+            OnPlayerDied();
+            //play a sound
+        }
+    }
+}
